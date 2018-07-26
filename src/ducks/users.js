8ba0ae89150378328701b7e58d5eac74f0be1080
Josiah Maina <josiah5115@gmail.com>
2018-07-26 16:13:43 -0600

    intial state

--- src/ducks/users.js
+++ src/ducks/users.js
@@ -1,7 +1,23 @@
+
 const initalState = {
   user: {}
+};
+
+const  UPDATE_USER_DATA = 'UPDATE_USER_DATA';
+
+export function updateUserData() {
+  return {
+    type: UPDATE_USER_DATA,
+    payload: user
+  }
 }
 
-export default function reducer (state, action) {
-  return state;
+export default function reducer(state = initalState, action) {
+  switch (action.type) {
+    case UPDATE_USER_DATA:
+    return Object.assign({}, state, { user: action.user });
+
+    default:
+      return state;
+  }
 }
