dc54d23ad00f9f27d7ce5b39dcaa90c764c10f30
Josiah Maina <josiah5115@gmail.com>
2018-07-26 11:00:44 -0600

    login fn

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,13 +4,17 @@ import logo from './communityBank.svg';
 import './Login.css'
 
 
+export default class Login extends Component {
 
-login() {
-  
+  login() {
+    let { REACT_APP_DOMAIN, REACT_APP_CLIENT_ID } = process.env;
+
+    let url = `${window.location.origin}/auth/callback`
+
+
+    window.location = `https//${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${}&response_type=code`
 }
 
-
-export default class Login extends Component {
   render() {
     return (
       <div className="App">
