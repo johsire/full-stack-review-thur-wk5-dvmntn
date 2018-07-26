dddd3235db16ce4efe661675c0ded0c59e4498f4
Josiah Maina <josiah5115@gmail.com>
2018-07-26 14:10:00 -0600

    using massive to connect to the db

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 const express = require('express');
  session = require('express-session');
  axios = require('axios');
+ massive = require('massive');
 
 
  const app = express();
 
 
- const { SERVER_PORT, REACT_APP_DOMAIN, REACT_APP_CLIENT_ID, CLIENT_SECRET, SESSION_SECRET } = process.env;
+ const { SERVER_PORT, REACT_APP_DOMAIN, REACT_APP_CLIENT_ID, CLIENT_SECRET, SESSION_SECRET, CONNECTION_STRING } = process.env;
 
+ massive(CONNECTION_STRING).then(db => {
+  app.set('db', db)
+ });
 
  app.use(session ({
   secret: SESSION_SECRET,
