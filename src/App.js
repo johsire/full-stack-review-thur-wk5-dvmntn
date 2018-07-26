9d9408c2e4e28c7f782975fcbb17e2c08ebf59a0
Josiah Maina <josiah5115@gmail.com>
2018-07-26 10:17:22 -0600

    full-stack-review init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <HashRouter>
+
+      </HashRouter>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
